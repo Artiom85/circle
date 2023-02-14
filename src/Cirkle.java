1b0bf7b6a9cb6c323740a4b7151d5bdaee976043
Artiom85 <stratulatartemii2012@gmail.com>
2023-02-14 16:53:53 +0000

    exe. cu cirkle

---
+++ src/Cirkle.java
@@ -0,0 +1,18 @@
+
+
+public class Cirkle {
+
+    double radius;
+    public double getRadius() {
+        return calculateArea();
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double calculateArea() {
+        return (radius * 2) * 3.14;
+    }
+
+
+        }
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       Cirkle cerc = new Cirkle();
+       cerc.setRadius(3);
+        System.out.println("aria= " + cerc.getRadius());
+    }
+
+}
